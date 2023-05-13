989adf8dc2a69b02a69b78c390bc2f6be793e265
Wertplok <wert32473@gmail.com>
2023-05-13 13:11:21 +0300

    add task 12

---
+++ task-12.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, surname, year, grades=dict()) -> None:
+        self.name = name
+        self.surname = surname
+        self.year = year
+        self.grades = grades
+
+    def average_grade(self):
+        return sum(self.grades.values())/len(self.grades.values())
+
+man = Student("Иван", "Иванов", 2, {"физика":4, "инфоматика":5})
+print(man.average_grade())
