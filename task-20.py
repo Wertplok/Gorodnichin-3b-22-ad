c4a138c45f4551f1da3c87843044a1c3c4927d72
Wertplok <wert32473@gmail.com>
2023-05-20 12:54:34 +0300

    fis task 20

--- task-20.py
+++ task-20.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-my_list = [randint(0, 100) for _ in range(10)]
+my_list = [randint(1, 100) for _ in range(10)]
 
 print(f"Массив {my_list = }")
 print(f"Max = {max(my_list)}")
