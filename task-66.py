c63d691fa6b68de6724ad5476cdb022470d99284
Wertplok <wert32473@gmail.com>
2023-07-01 18:20:48 +0300

    add task 66

---
+++ task-66.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+
+DATABASE_NAME = "students"
+FIELDS = {"name":"TEXT",
+          "age":"INTEGER",
+          "average_mark": "REAL"}
+SELECTED_FIELD = ""
+VALUES = [('James', 18, 4.8),
+          ('John', 19, 4.6),
+          ('Robert', 20, 4.5),
+          ('Michael', 21, 4.1),
+          ('William', 22, 3.9),
+]
+CONDITION = ""
+
+
+conn = sqlite3.connect(f"{DATABASE_NAME}_database.db")
+
+cursor = conn.cursor()
+cursor.execute(f'''
+CREATE TABLE IF NOT EXISTS {DATABASE_NAME}
+(id INTEGER PRIMARY KEY, {', '.join([f"{key} {value}" for key, value in FIELDS.items()])})
+''')
+cursor.executemany(f"INSERT INTO {DATABASE_NAME} ({', '.join(FIELDS.keys())}) VALUES ({', '.join(['?'] * len(FIELDS))})", VALUES)
+conn.commit()
+
+cursor.execute(f"SELECT {SELECTED_FIELD or '*'} FROM {DATABASE_NAME} {f'WHERE {CONDITION}' if CONDITION else ''}")
+columns = [*zip(*cursor.description)][0]
+result = [columns] + cursor.fetchall()
+lengths = []
+
+for indx, column in enumerate(columns):
+    cursor.execute(f"SELECT max(length({column})) FROM {DATABASE_NAME} {f'WHERE {CONDITION}' if CONDITION else ''}")
+    lengths.append(max(cursor.fetchone()[0], len(column)))
+
+[print('   '.join([f"{row[i]:^{lengths[i]}}" for i in range(len(columns))])) for row in result]
+
+conn.close()
